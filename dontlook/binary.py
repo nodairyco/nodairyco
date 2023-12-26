fcc9a4b84f2338bf9a81b7c94bebf1a7fc63f3e3
NoDairy.co <151561762+nodairyco@users.noreply.github.com>
2023-12-26 14:10:21 +0400

    Add files via upload

---
+++ dontlook/binary.py
@@ -0,0 +1,264 @@
+dictionary = {
+       "A": '01000001',
+       "B": '01000010',
+       "C": '01000011',
+       "D": '01000100',
+       "E": '01000101',
+       "F": '01000110',
+       "G": '01000111',
+       "H": '01001000',
+       "I": '01001001',
+       "J": '01001010',
+       "K": '01001011',
+       "L": '01001100',
+       "M": '01001101',
+       "N": '01001110',
+       "O": '01001111',
+       "P": '01010000',
+       "Q": '01010001',
+       "R": '01010010',
+       "S": '01010011',
+       "T": '01010100',
+       "U": '01010101',
+       "V": '01010110',
+       "W": '01010111',
+       "X": '01011000',
+       "Y": '01011001',
+       "Z": '01011010',
+       "a": '01100001',
+       "b": '01100010',
+       "c": '01100011',
+       "d": '01100100',
+       "e": '01100101',
+       "f": '01100110',
+       "g": '01100111',
+       "h": '01101000',
+       "i": '01101001',
+       "j": '01101010',
+       "k": '01101011',
+       "l": '01101100',
+       "m": '01101101',
+       "n": '01101110',
+       "o": '01101111',
+       "p": '01110000',
+       "q": '01110001',
+       "r": '01110010',
+       "s": '01110011',
+       "t": '01110100',
+       "u": '01110101',
+       "v": '01110110',
+       "w": '01110111',
+       "x": '01111000',
+       "y": '01111001',
+       "z": '01111010',
+       "!": '00100001',
+       ".": '00101110',
+       ",": '00101100',
+       "?": '00111111',
+       " ": ' ',
+       "1": "00000001",
+       "2": '00000010',
+       "3": '00000011',
+       "4": '00000100',
+       '5': '00000101',
+       '6': '00000110',
+       '7': '00000111',
+       '8': '00001000',
+       '9': '00001001',
+       '0': '00000000',
+       "'": '00100111',
+       '"': '00100010',
+       "(": '00101000',
+       ")": '00101001'
+       }
+sentence = input("Input a sentencetence:")
+sl1 = slice(0, 1)
+sl2 = slice(1, 2)
+sl3 = slice(2, 3)
+sl4 = slice(3, 4)
+sl5 = slice(4, 5)
+sl6 = slice(5, 6)
+sl7 = slice(6, 7)
+sl8 = slice(7, 8)
+sl9 = slice(8, 9)
+sl10 = slice(9, 10)
+sl11 = slice(10, 11)
+sl12 = slice(11, 12)
+sl13 = slice(12, 13)
+sl14 = slice(13, 14)
+sl15 = slice(14, 15)
+sl16 = slice(15, 16)
+sl17 = slice(16, 17)
+sl18 = slice(17, 18)
+sl19 = slice(18, 19)
+sl20 = slice(19, 20)
+sl21 = slice(20, 21)
+sl22 = slice(21, 22)
+sl23 = slice(22, 23)
+sl24 = slice(23, 24)
+sl25 = slice(24, 25)
+sl26 = slice(25, 26)
+sl27 = slice(26, 27)
+sl28 = slice(27, 28)
+sl29 = slice(28, 29)
+sl30 = slice(29, 30) 
+if len(sentence) == 1:
+    print(((dictionary[sentence[sl1]])))
+elif len(sentence) == 2:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])))
+elif len(sentence) == 3:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])))
+elif len(sentence) == 4:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])))
+elif len(sentence) == 5:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])))
+elif len(sentence) == 6:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])))
+elif len(sentence) == 7:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])))
+elif len(sentence) == 8:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])), 
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])))
+elif len(sentence) == 9:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])))
+elif len(sentence) == 10:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])))
+elif len(sentence) == 11:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])), 
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])))
+elif len(sentence) == 12:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])))
+elif len(sentence) == 13: 
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])), 
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])))
+elif len(sentence) == 14: 
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])))
+elif len(sentence) == 15:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])), 
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])))
+elif len(sentence) == 16:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])), 
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])), 
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])), 
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])))
+elif len(sentence) == 17:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])), 
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])))
+elif len(sentence) == 18:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])))
+elif len(sentence) == 19:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])), 
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])))
+elif len(sentence) == 20:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])))
+elif len(sentence) == 21:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])))
+elif len(sentence) == 22:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])))
+elif len(sentence) == 23:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])))
+elif len(sentence) == 24:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])))
+elif len(sentence) == 25:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])),
+          ((dictionary[sentence[sl25]])))
+elif len(sentence) == 26:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])),
+          ((dictionary[sentence[sl25]])), ((dictionary[sentence[sl26]])))
+elif len(sentence) == 27:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])),
+          ((dictionary[sentence[sl25]])), ((dictionary[sentence[sl26]])), ((dictionary[sentence[sl27]])))
+elif len(sentence) == 28:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])),
+          ((dictionary[sentence[sl25]])), ((dictionary[sentence[sl26]])), ((dictionary[sentence[sl27]])), ((dictionary[sentence[sl28]])))
+elif len(sentence) == 29:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])),
+          ((dictionary[sentence[sl25]])), ((dictionary[sentence[sl26]])), ((dictionary[sentence[sl27]])), ((dictionary[sentence[sl28]])),
+          ((dictionary[sentence[sl29]])))
+elif len(sentence) == 30:
+    print(((dictionary[sentence[sl1]])), ((dictionary[sentence[sl2]])), ((dictionary[sentence[sl3]])), ((dictionary[sentence[sl4]])),
+          ((dictionary[sentence[sl5]])), ((dictionary[sentence[sl6]])), ((dictionary[sentence[sl7]])), ((dictionary[sentence[sl8]])),
+          ((dictionary[sentence[sl9]])), ((dictionary[sentence[sl10]])), ((dictionary[sentence[sl11]])), ((dictionary[sentence[sl12]])),
+          ((dictionary[sentence[sl13]])), ((dictionary[sentence[sl14]])), ((dictionary[sentence[sl15]])), ((dictionary[sentence[sl16]])),
+          ((dictionary[sentence[sl17]])), ((dictionary[sentence[sl18]])), ((dictionary[sentence[sl19]])), ((dictionary[sentence[sl20]])),
+          ((dictionary[sentence[sl21]])), ((dictionary[sentence[sl22]])), ((dictionary[sentence[sl23]])), ((dictionary[sentence[sl24]])),
+          ((dictionary[sentence[sl25]])), ((dictionary[sentence[sl26]])), ((dictionary[sentence[sl27]])), ((dictionary[sentence[sl28]])),
+          ((dictionary[sentence[sl29]])), ((dictionary[sentence[sl30]])))
+else:
+    print("Too big ahh sentencetence")
